301ee43a369d0e04de368bfc57910e4c117d6695
eulu <anegamega@gmail.com>
2025-03-10 21:22:49 +0200

    Add update book endpoint

--- src/main/java/org/eulu/bookshop/controller/BookController.java
+++ src/main/java/org/eulu/bookshop/controller/BookController.java
@@ -8,6 +8,7 @@ import org.eulu.bookshop.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,13 @@ public class BookController {
     @GetMapping
     public List<BookDto> getAll() {
         return bookService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(
+            @PathVariable Long id,
+            @RequestBody CreateBookRequestDto requestDto
+    ) {
+        return bookService.update(id, requestDto);
     }
 }
