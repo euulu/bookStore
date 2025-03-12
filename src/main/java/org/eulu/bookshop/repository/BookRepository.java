742ffa7c9c32dd60861760823701ec0fd60847c8
eulu <anegamega@gmail.com>
2025-03-12 22:57:41 +0200

    Extend BookRepository with JpaSpecificationExecutor

--- src/main/java/org/eulu/bookshop/repository/BookRepository.java
+++ src/main/java/org/eulu/bookshop/repository/BookRepository.java
@@ -2,6 +2,7 @@ package org.eulu.bookshop.repository;
 
 import org.eulu.bookshop.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 }
