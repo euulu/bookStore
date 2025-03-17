234a0e2cffb03eafefd3bd31f209f3b0636a334b
eulu <anegamega@gmail.com>
2025-03-17 20:13:10 +0200

    Add field validation annotations to the book dto classes

--- src/main/java/org/eulu/bookshop/dto/BookDto.java
+++ src/main/java/org/eulu/bookshop/dto/BookDto.java
@@ -1,15 +1,29 @@
 package org.eulu.bookshop.dto;
 
 import java.math.BigDecimal;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class BookDto {
+    @NotNull
     private Long id;
+    @NotNull
+    @Length(max = 255)
     private String title;
+    @NotNull
+    @Length(max = 255)
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
+    @Max(999999999)
     private BigDecimal price;
+    @Length(max = 500)
     private String description;
     private String coverImage;
 }
