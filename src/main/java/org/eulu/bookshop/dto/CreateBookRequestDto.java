fabf2998988a0bb32bfd5a5b9c275ab33d2afe98
eulu <anegamega@gmail.com>
2025-03-18 10:57:33 +0200

    Use right annotations for CreateBookRequestDto fields

--- src/main/java/org/eulu/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/org/eulu/bookshop/dto/CreateBookRequestDto.java
@@ -1,12 +1,9 @@
 package org.eulu.bookshop.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
