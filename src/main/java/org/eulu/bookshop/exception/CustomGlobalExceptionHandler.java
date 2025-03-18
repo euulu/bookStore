7643193e374e44b1563a59486c02af77a46d02ed
eulu <anegamega@gmail.com>
2025-03-18 11:01:12 +0200

    Fix: remove body from handleValidationException ResponseEntity

--- src/main/java/org/eulu/bookshop/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/org/eulu/bookshop/exception/CustomGlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.eulu.bookshop.exception;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -12,19 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomGlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<FieldValidationErrorResponse> handleValidationException(
+    public ResponseEntity<List<String>> handleValidationException(
             MethodArgumentNotValidException ex
     ) {
-        List<String> errors = ex.getBindingResult()
-                .getAllErrors()
+        List<String> errors = ex.getBindingResult().getAllErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
-        FieldValidationErrorResponse body = new FieldValidationErrorResponse(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST,
-                errors
-        );
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
