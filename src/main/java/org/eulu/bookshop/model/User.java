35fc7c69d89791262b64bfac0022592515b915a7
eulu <anegamega@gmail.com>
2025-03-24 15:18:30 +0200

    Replace @Data with @Getter, @Setter in User model

--- src/main/java/org/eulu/bookshop/model/User.java
+++ src/main/java/org/eulu/bookshop/model/User.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
