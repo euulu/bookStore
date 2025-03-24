427b276d517bf99cacef8cb7a8a11fc4de3488e2
eulu <anegamega@gmail.com>
2025-03-24 19:57:05 +0200

    Add Role entity

---
+++ src/main/java/org/eulu/bookshop/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package org.eulu.bookshop.repository;
+
+import org.eulu.bookshop.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
