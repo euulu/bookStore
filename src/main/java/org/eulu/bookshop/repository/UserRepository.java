d068c94587efbbbfb5c73ffcdfae9884944004f9
eulu <anegamega@gmail.com>
2025-03-24 19:30:17 +0200

    Add find user by email repository method

--- src/main/java/org/eulu/bookshop/repository/UserRepository.java
+++ src/main/java/org/eulu/bookshop/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package org.eulu.bookshop.repository;
 
+import java.util.Optional;
 import org.eulu.bookshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByEmailEqualsIgnoreCase(String email);
+
+    Optional<User> findUserByEmailEqualsIgnoreCase(String email);
 }
