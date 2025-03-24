787e35d333f77792484f4c24cb219166e41a4db9
eulu <anegamega@gmail.com>
2025-03-24 19:57:05 +0200

    Add Role entity

--- src/main/java/org/eulu/bookshop/repository/RoleRepository.java
+++ src/main/java/org/eulu/bookshop/repository/RoleRepository.java
@@ -4,5 +4,8 @@ import org.eulu.bookshop.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+<<<<<<< HEAD
     Role findByName(Role.RoleName name);
+=======
+>>>>>>> 10eda71 (Add Role entity)
 }
