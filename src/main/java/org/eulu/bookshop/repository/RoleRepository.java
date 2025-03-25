66b5df5a9aa5c66d2c4939ab0c6e48d45b94d044
eulu <anegamega@gmail.com>
2025-03-25 19:24:32 +0200

    Set default USER role when registering a new user

--- src/main/java/org/eulu/bookshop/repository/RoleRepository.java
+++ src/main/java/org/eulu/bookshop/repository/RoleRepository.java
@@ -1,7 +1,9 @@
 package org.eulu.bookshop.repository;
 
+import java.util.Set;
 import org.eulu.bookshop.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Set<Role> findByName(Role.RoleName name);
 }
