ad93aa9f1ff499fb708ae4119e04e0db720272b7
eulu <anegamega@gmail.com>
2025-03-25 19:24:32 +0200

    Set default USER role when registering a new user

--- src/main/java/org/eulu/bookshop/service/UserServiceImpl.java
+++ src/main/java/org/eulu/bookshop/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package org.eulu.bookshop.service;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.eulu.bookshop.dto.user.CreateUserRequestDto;
 import org.eulu.bookshop.dto.user.UserDto;
 import org.eulu.bookshop.exception.RegistrationException;
 import org.eulu.bookshop.mapper.UserMapper;
+import org.eulu.bookshop.model.Role;
 import org.eulu.bookshop.model.User;
+import org.eulu.bookshop.repository.RoleRepository;
 import org.eulu.bookshop.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
@@ -25,6 +29,8 @@ public class UserServiceImpl implements UserService {
         }
         User user = userMapper.toEntity(createUserRequestDto);
         user.setPassword(passwordEncoder.encode(createUserRequestDto.password()));
+        Set<Role> roles = roleRepository.findByName(Role.RoleName.USER);
+        user.setRoles(roles);
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
