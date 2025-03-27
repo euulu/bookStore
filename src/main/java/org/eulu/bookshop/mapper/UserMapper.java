fbfb3a9917671951b0563fda3bdf431d93586d2e
eulu <anegamega@gmail.com>
2025-03-27 09:02:05 +0200

    Remove @Mapping annotation for unmapped fields in UserMapper

--- src/main/java/org/eulu/bookshop/mapper/UserMapper.java
+++ src/main/java/org/eulu/bookshop/mapper/UserMapper.java
@@ -5,14 +5,10 @@ import org.eulu.bookshop.dto.user.CreateUserRequestDto;
 import org.eulu.bookshop.dto.user.UserDto;
 import org.eulu.bookshop.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
     UserDto toDto(User user);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "roles", ignore = true)
-    @Mapping(target = "authorities", ignore = true)
     User toEntity(CreateUserRequestDto userRequestDto);
 }
