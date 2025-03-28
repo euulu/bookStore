1d106a55faa45e7cb6437308e378cb0df93dc592
eulu <anegamega@gmail.com>
2025-03-28 18:47:51 +0200

    Fix CategoryMapper config annotation

--- src/main/java/org/eulu/bookshop/mapper/CategoryMapper.java
+++ src/main/java/org/eulu/bookshop/mapper/CategoryMapper.java
@@ -1,12 +1,13 @@
 package org.eulu.bookshop.mapper;
 
+import org.eulu.bookshop.config.MapperConfig;
 import org.eulu.bookshop.dto.category.CategoryDto;
 import org.eulu.bookshop.dto.category.CategoryRequestDto;
 import org.eulu.bookshop.model.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(config = Mapper.class)
+@Mapper(config = MapperConfig.class)
 public interface CategoryMapper {
     CategoryDto toDto(Category category);
 
