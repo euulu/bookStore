9cc74a54c12c2e676feb457cf7f4d0be26351e0b
eulu <anegamega@gmail.com>
2025-03-28 18:49:09 +0200

    Implement Category controller endpoints

--- src/main/java/org/eulu/bookshop/repository/BookRepository.java
+++ src/main/java/org/eulu/bookshop/repository/BookRepository.java
@@ -1,8 +1,13 @@
 package org.eulu.bookshop.repository;
 
+import java.util.Set;
 import org.eulu.bookshop.model.Book;
+import org.eulu.bookshop.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    Page<Book> findBooksByCategoriesContaining(Set<Category> categories, Pageable pageable);
 }
