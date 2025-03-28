349c78f55f0c838ec9bfa7c49bc4b913ab107b94
eulu <anegamega@gmail.com>
2025-03-28 16:51:39 +0200

    Rename one of the category dtos with more suitable name

--- src/main/java/org/eulu/bookshop/dto/category/CategoryRequestDto.java
+++ src/main/java/org/eulu/bookshop/dto/category/CategoryRequestDto.java
@@ -2,7 +2,7 @@ package org.eulu.bookshop.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UpdateCategoryRequestDto(
+public record CategoryRequestDto(
         @NotBlank
         String name,
         String description
