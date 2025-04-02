4fbec0a6c6fd0e0dd91f11d0a51457828466e7d4
eulu <anegamega@gmail.com>
2025-04-02 22:21:12 +0300

    Add ShoppingCart controller addBookToShoppingCart method

---
+++ src/main/java/org/eulu/bookshop/controller/ShoppingCartController.java
@@ -0,0 +1,45 @@
+package org.eulu.bookshop.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.eulu.bookshop.dto.cartitem.CreateCartItemRequestDto;
+import org.eulu.bookshop.dto.shoppingcart.ShoppingCartDto;
+import org.eulu.bookshop.mapper.ShoppingCartMapper;
+import org.eulu.bookshop.model.ShoppingCart;
+import org.eulu.bookshop.service.ShoppingCartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+@RequiredArgsConstructor
+@Tag(name = "Shopping cart", description = "Operations related to shopping cart management")
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final ShoppingCartMapper shoppingCartMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(
+            summary = "Add cart item to the shopping cart",
+            description = "Add book id and books quantity to the "
+                    + "currently logged in users shopping cart"
+    )
+    public ShoppingCartDto addBookToShoppingCart(
+            Authentication authentication,
+            @RequestBody @Valid CreateCartItemRequestDto cartItemRequest
+    ) {
+        ShoppingCart shoppingCart = shoppingCartService
+                .addCartItem(authentication, cartItemRequest);
+        return shoppingCartMapper.toDto(shoppingCart);
+    }
+}
