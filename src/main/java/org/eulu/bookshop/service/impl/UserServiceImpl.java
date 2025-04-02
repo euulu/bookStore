aba3883b5b4c177ec1639ffcb077e7e9b76099cd
eulu <anegamega@gmail.com>
2025-04-02 22:22:03 +0300

    Update UserService to initialize and add a ShoppingCart on user registration

--- src/main/java/org/eulu/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/org/eulu/bookshop/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.eulu.bookshop.service.impl;
 
+import jakarta.transaction.Transactional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.eulu.bookshop.dto.user.CreateUserRequestDto;
@@ -7,8 +8,10 @@ import org.eulu.bookshop.dto.user.UserDto;
 import org.eulu.bookshop.exception.RegistrationException;
 import org.eulu.bookshop.mapper.UserMapper;
 import org.eulu.bookshop.model.Role;
+import org.eulu.bookshop.model.ShoppingCart;
 import org.eulu.bookshop.model.User;
 import org.eulu.bookshop.repository.RoleRepository;
+import org.eulu.bookshop.repository.ShoppingCartRepository;
 import org.eulu.bookshop.repository.UserRepository;
 import org.eulu.bookshop.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,10 +22,12 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
     @Override
+    @Transactional
     public UserDto register(CreateUserRequestDto createUserRequestDto) {
         if (userRepository.existsUserByEmailEqualsIgnoreCase(createUserRequestDto.email())) {
             throw new RegistrationException("User with email: "
@@ -32,7 +37,10 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(createUserRequestDto.password()));
         Role roleUser = roleRepository.findByName(Role.RoleName.ROLE_USER);
         user.setRoles(Set.of(roleUser));
-        User savedUser = userRepository.save(user);
-        return userMapper.toDto(savedUser);
+        ShoppingCart cart = new ShoppingCart();
+        cart.setUser(user);
+        shoppingCartRepository.save(cart);
+        userRepository.save(user);
+        return userMapper.toDto(user);
     }
 }
