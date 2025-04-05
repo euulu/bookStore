c44548511f3540ced76ce034964b26ba29b71e67
eulu <anegamega@gmail.com>
2025-04-05 22:56:20 +0300

    Eagerly fetch both cartItems and books when fetching shopping cart

--- src/main/java/org/eulu/bookshop/repository/ShoppingCartRepository.java
+++ src/main/java/org/eulu/bookshop/repository/ShoppingCartRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @EntityGraph(attributePaths = {"cartItems.book"})
+    @EntityGraph(attributePaths = {"cartItems", "cartItems.book"})
     Optional<ShoppingCart> findShoppingCartByUser(User user);
 }
