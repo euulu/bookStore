7c8839514cde4a176e8499af6902fedee8687e6b
eulu <anegamega@gmail.com>
2025-04-29 19:49:55 +0300

    Initialize categories list with new HashSet in the Book entity

--- src/main/java/org/eulu/bookshop/model/Book.java
+++ src/main/java/org/eulu/bookshop/model/Book.java
@@ -10,6 +10,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
@@ -42,5 +43,5 @@ public class Book {
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 }
