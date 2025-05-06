a4c899586279e6d97d9949ea1bd224ae194ce230
eulu <anegamega@gmail.com>
2025-05-06 14:58:01 +0300

    Make OrderItem-Order association non-nullable

--- src/main/java/org/eulu/bookshop/model/OrderItem.java
+++ src/main/java/org/eulu/bookshop/model/OrderItem.java
@@ -21,7 +21,7 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
